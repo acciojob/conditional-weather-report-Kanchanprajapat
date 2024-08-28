8520b38dae0e44baacb38c2c486def773a0d556d
kanchan <kp4307699@gmail.com>
2024-08-28 10:53:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from './WeatherDisplay';  // Correct import
 
 const App = () => {
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    // Update weather data with the provided input
+    const weatherarr = [
+        { temperature: 25, conditions: "Sunny" },
+    ];
+    setWeather(weatherarr);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
